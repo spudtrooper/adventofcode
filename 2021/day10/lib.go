c29473ca9d092f8d256788ed7b6c353a3408bd3e
Jeff Palm <jeff@jeffpalm.com>
2021-12-10 07:17:43 -0500

    day 10 part 2

--- 2021/day10/lib.go
+++ 2021/day10/lib.go
@@ -55,49 +55,35 @@ func (s *stack) Pop() (string, bool) {
 	return element, true
 }
 
-func (s *stack) Peek() (string, bool) {
-	if s.Empty() {
-		return "", false
-	}
-	index := len(*s) - 1
-	element := (*s)[index]
-	return element, true
-}
-
-var loud = false
-
-func firstIllegalChar(chunk string) string {
-	var s stack
-
-	demand := func(want, right string, i int) (err bool) {
-		if loud {
-			log.Printf("%s:%d: demand want=%s right=%s stack=%v", chunk, i, want, right, s)
-		}
-		p, more := s.Pop()
-		if !more {
-			log.Fatalf("should have more pos:%d: %s", i, s)
-		}
-		if p != want {
-			err = true
-			log.Printf("Expected %s, but found %s instead at %d in %s", rightMatching[p], right, i, chunk)
-		}
-		return
-	}
+func firstIllegalChar(s *stack, chunk string) string {
+	// demand := func(want, right string, i int) (err bool) {
+	// 	p, more := s.Pop()
+	// 	if !more {
+	// 		log.Fatalf("should have more pos:%d: %s", i, s)
+	// 	}
+	// 	if p != want {
+	// 		err = true
+	// 		log.Printf("Expected %s, but found %s instead at %d in %s", rightMatching[p], right, i, chunk)
+	// 	}
+	// 	return
+	// }
 
 	for i, c := range strings.Split(chunk, "") {
-		if loud {
-			log.Printf("%s:%d: have c=%s stack=%v", chunk, i, c, s)
-		}
 		switch c {
 		case "(", "[", "{", "<":
-			if loud {
-				log.Printf("%s:%d: pushing c=%s", chunk, i, c)
-			}
 			s.Push(c)
 		case ")", "]", "}", ">":
-			if err := demand(leftMatching[c], c, i); err {
+			p, more := s.Pop()
+			if !more {
+				log.Fatalf("should have more pos:%d: %s", i, s)
+			}
+			if want := leftMatching[c]; p != want {
+				log.Printf("Expected %s, but found %s instead at %d in %s", rightMatching[p], c, i, chunk)
 				return c
 			}
+			// if err := demand(leftMatching[c], c, i); err {
+			// 	return c
+			// }
 		default:
 			log.Fatalf("Invalid character at pos %d: %s", i, chunk)
 		}
@@ -108,68 +94,29 @@ func firstIllegalChar(chunk string) string {
 func Part1(input string) int {
 	var score int
 	for _, line := range must.ReadStrings(input) {
-		if char := firstIllegalChar(line); char != "" {
-			score += points[char]
+		var s stack
+		if c := firstIllegalChar(&s, line); c != "" {
+			score += points[c]
 		}
-
 	}
-
 	return score
-}
-
-func firstIllegalCharWithStack(s *stack, chunk string) string {
-	demand := func(want, right string, i int) (err bool) {
-		if loud {
-			log.Printf("%s:%d: demand want=%s right=%s stack=%v", chunk, i, want, right, s)
-		}
-		p, more := s.Pop()
-		if !more {
-			log.Fatalf("should have more pos:%d: %s", i, s)
-		}
-		if p != want {
-			err = true
-			log.Printf("Expected %s, but found %s instead at %d in %s", rightMatching[p], right, i, chunk)
-		}
-		return
-	}
-
-	for i, c := range strings.Split(chunk, "") {
-		if loud {
-			log.Printf("%s:%d: have c=%s stack=%v", chunk, i, c, s)
-		}
-		switch c {
-		case "(", "[", "{", "<":
-			if loud {
-				log.Printf("%s:%d: pushing c=%s", chunk, i, c)
-			}
-			s.Push(c)
-		case ")", "]", "}", ">":
-			if err := demand(leftMatching[c], c, i); err {
-				return c
-			}
-		default:
-			log.Fatalf("Invalid character at pos %d: %s", i, chunk)
-		}
-	}
-	return ""
 }
 
 func Part2(input string) int {
 	var scores []int
 	for _, line := range must.ReadStrings(input) {
 		var s stack
-		if char := firstIllegalCharWithStack(&s, line); char == "" {
-			log.Printf("stack: %s", s)
+		if c := firstIllegalChar(&s, line); c == "" {
 			complete := ""
 			var score int
 			for {
-				c, more := s.Pop()
+				p, more := s.Pop()
 				if !more {
 					break
 				}
 				score *= 5
-				score += autocompletePoints[rightMatching[c]]
-				complete += rightMatching[c]
+				score += autocompletePoints[rightMatching[p]]
+				complete += rightMatching[p]
 			}
 			scores = append(scores, score)
 			log.Printf("%s - %d total points.", complete, score)
@@ -178,6 +125,5 @@ func Part2(input string) int {
 	}
 	sort.Ints(scores)
 	log.Printf("scores: %v", scores)
-	middle := scores[len(scores)/2]
-	return middle
+	return scores[len(scores)/2]
 }
