9305793cb8cfeb82f2451197fbf1a5351362ddee
Jeff Palm <jeff@jeffpalm.com>
2021-12-13 08:15:47 -0500

    day 13 part 2

--- 2021/day13/lib/lib.go
+++ 2021/day13/lib/lib.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"regexp"
 	"strings"
@@ -25,25 +26,35 @@ type paper struct {
 	width, height int
 }
 
-func (p paper) Debug() string {
+func (p paper) Debug(f fold) string {
 	arr := make([][]string, p.height)
 	for y := 0; y < p.height; y++ {
 		arr[y] = make([]string, p.width)
 		for x := range arr[y] {
 			arr[y][x] = "."
+			if f.along == "y" {
+				if y == f.val {
+					arr[y][x] = "-"
+				}
+			} else {
+				if x == f.val {
+					arr[y][x] = "|"
+				}
+
+			}
 		}
 	}
 	var i int
 	for _, c := range p.coords {
 		if c.y < 0 || c.y >= p.height || c.x < 0 || c.x >= p.width {
-			log.Printf("Invalid width=%d height=%d coords[%d]: %+v", p.width, p.height, i, c)
+			log.Fatalf("Invalid width=%d height=%d coords[%d]: %+v", p.width, p.height, i, c)
 		}
 		arr[c.y][c.x] = "#"
 		i++
 	}
 	var buf bytes.Buffer
-	for _, row := range arr {
-		buf.WriteString(strings.Join(row, ""))
+	for i, row := range arr {
+		buf.WriteString(fmt.Sprintf("%3d %s", i, strings.Join(row, "")))
 		buf.WriteString("\n")
 	}
 	return buf.String()
@@ -134,31 +145,86 @@ func Part1(input string) int {
 		return new
 	}
 
-	// for i, f := range folds {
-	// 	p = fold(p, f)
-	// 	log.Printf("paper after %d folds\n", i+1)
-	// 	if true || p.width < 200 {
-	// 		log.Printf("\n%v", p.Debug())
-	// 	}
-	// 	log.Printf("paper after %d folds", i+1)
-	// 	log.Printf("<br/>")
-	// 	for _, c := range p.coords {
-	// 		log.Printf("%+v", c)
-	// 	}
-	// }
-
 	p = fold(p, folds[0])
-	// p = fold(p, folds[1])
 
 	return len(p.coords)
 }
 
 func Part2(input string) int {
+	var folds []fold
+	var coords []coord
+	var maxX, maxY int
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	hash := func(p paper, c coord) int {
+		return p.width*c.y + c.x
+	}
+
 	for _, line := range must.ReadLines(input) {
-		// TODO
-		if false {
-			log.Println(line)
+		if line == "" {
+			continue
+		}
+		if m := foldRE.FindStringSubmatch(line); len(m) == 3 {
+			along, val := m[1], must.Atoi(m[2])
+			folds = append(folds, fold{along, val})
+		} else {
+			parts := must.SplitInts(line, ",")
+			x, y := parts[0], parts[1]
+			coords = append(coords, coord{x: x, y: y})
+			maxX = max(maxX, x)
+			maxY = max(maxY, y)
 		}
 	}
-	return -1
+
+	initHeight, initWidth := maxY+1, maxX+1
+
+	log.Printf("initWidth: %v", initWidth)
+	log.Printf("initHeight: %v", initHeight)
+	log.Printf("folds: %+v", folds)
+	log.Printf("coords: %+v", coords)
+
+	makePaper := func(height, width int) paper {
+		return paper{height: height, width: width, coords: map[int]coord{}}
+	}
+
+	p := makePaper(initHeight, initWidth)
+	for _, c := range coords {
+		p.coords[hash(p, c)] = c
+	}
+
+	fold := func(p paper, f fold) paper {
+		if f.along == "y" {
+			new := makePaper(f.val, p.width)
+			for _, c := range p.coords {
+				if c.y > f.val {
+					c.y = f.val - (c.y - f.val)
+				}
+				new.coords[hash(p, c)] = c
+			}
+			return new
+		}
+
+		new := makePaper(p.height, f.val)
+		for _, c := range p.coords {
+			if c.x > f.val {
+				c.x = f.val - (c.x - f.val)
+			}
+			new.coords[hash(p, c)] = c
+		}
+
+		return new
+	}
+
+	for _, f := range folds {
+		p = fold(p, f)
+		log.Printf("\n%v", p.Debug(f))
+	}
+
+	return len(p.coords)
 }
--- 2021/day13/lib/lib_test.go
+++ 2021/day13/lib/lib_test.go
@@ -37,13 +37,13 @@ func TestPart2(t *testing.T) {
 		{
 			name:  "testinput",
 			input: "testdata/testinput.txt",
-			want:  -1, // TODO
+			want:  16,
 		},
-		// {
-		// 	name:  "part2",
-		//	input: "testdata/input.txt",
-		//	want:  -1, // TODO
-		// },
+		{
+			name:  "part2",
+			input: "testdata/input.txt",
+			want:  92,
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
