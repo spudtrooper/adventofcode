c157c98ace46b12694443bed97b501467a824051
Jeff Palm <jeff@jeffpalm.com>
2021-12-16 06:53:03 -0500

    hack for day 15 part 2

---
+++ 2021/day16/lib/lib.go
@@ -0,0 +1,27 @@
+package lib
+
+import (
+	"log"
+
+	"github.com/spudtrooper/adventofcode/common/must"
+)
+
+func Part1(input string) int {
+	for _, line := range must.ReadLines(input) {
+		// TODO
+		if false {
+			log.Println(line)
+		}
+	}
+	return -1
+}
+
+func Part2(input string) int {
+	for _, line := range must.ReadLines(input) {
+		// TODO
+		if false {
+			log.Println(line)
+		}
+	}
+	return -1
+}
--- 2021/day16/lib/lib_test.go
+++ 2021/day16/lib/lib_test.go
@@ -11,23 +11,18 @@ func TestPart1(t *testing.T) {
 		{
 			name:  "testinput",
 			input: "testdata/testinput.txt",
-			want:  40,
+			want:  -1, // TODO
 		},
-		{
-			name:  "part1",
-			input: "testdata/input.txt",
-			want:  710, // should be 702, but somehow got the right answer
-		},
-		{
-			name:  "part1",
-			input: "testdata/testinput-large.txt",
-			want:  320, // should be 315, but somehow got the right answer
-		},
+		// {
+		// 	name:  "part1",
+		//	input: "testdata/input.txt",
+		//	want:  -1, // TODO
+		// },
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			if want, got := tc.want, Part1(tc.input); want != got {
-				t.Errorf("day15 Part1: want(%d) != got(%d)", want, got)
+				t.Errorf("day16 Part1: want(%d) != got(%d)", want, got)
 			}
 		})
 	}
@@ -42,18 +37,18 @@ func TestPart2(t *testing.T) {
 		{
 			name:  "testinput",
 			input: "testdata/testinput.txt",
-			want:  320, // should be 315, but somehow got the right answer
+			want:  -1, // TODO
 		},
-		{
-			name:  "part2",
-			input: "testdata/input.txt",
-			want:  2956,
-		},
+		// {
+		// 	name:  "part2",
+		//	input: "testdata/input.txt",
+		//	want:  -1, // TODO
+		// },
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			if want, got := tc.want, Part2(tc.input); want != got {
-				t.Errorf("day15 Part2: want(%d) != got(%d)", want, got)
+				t.Errorf("day16 Part2: want(%d) != got(%d)", want, got)
 			}
 		})
 	}
